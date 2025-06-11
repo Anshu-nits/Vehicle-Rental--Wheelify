7cf3b045c33c2c59b20b94ce5a893a033baf3692
Anshu Raj Jaiswal <anshurajjaiswal2003@gmail.com>
2025-06-11 20:18:50 +0530

    Update index.js

--- Wheelify_Backend/index.js
+++ Wheelify_Backend/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://vehicle-rental-wheelify-frontend-wvdg.onrender.com', credentials: true }));
 
 
 //connect db
