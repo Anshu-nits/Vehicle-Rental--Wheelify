e92d6aa7adb13761a1ba9da615d5cca9a835dde4
Anshu Raj Jaiswal <anshurajjaiswal2003@gmail.com>
2025-06-12 01:30:25 +0530

    Update provider.js

--- Wheelify_Backend/Routes/provider.js
+++ Wheelify_Backend/Routes/provider.js
@@ -16,6 +16,7 @@ router.post(
     { name: "thumbnail", maxCount: 1 }, // Field for the vehicle thumbnail image
     { name: "ownershipProof", maxCount: 1 }, // Field for the ownership proof image
   ]),
+  auth,
   registerBike
 );
 
